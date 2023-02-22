28bffab86864198d9fee329fc4c4cc314678d547
kostad02 <kdautinishvili@gmail.com>
2023-02-23 02:11:19 +0400

    removed unused component

--- frontend/src/app/shared/index.ts
+++ frontend/src/app/shared/index.ts
@@ -2,7 +2,6 @@ export * from './codeeditor/codeeditor.component';
 export * from './footer/footer.component';
 export * from './header/header.component';
 export * from './example-code/example-code.component';
-export * from './external-source/external-source.component';
 export * from './subject-card/subject-card.component';
 export * from './error-output/error-output.component';
 export * from './scrollup/scrollup.component';
